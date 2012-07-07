8100d872b707c355f4de93f8bd1962c4e3052254
CrystalMoogle <mog-celes@hotmail.com>
2012-07-07 21:48:33 +0100

    Formatting~

--- AutoIdling Script.js
+++ AutoIdling Script.js
@@ -1,21 +1,20 @@
 //really simple script to make you idle on log on. Requested by [LD]Jirachier :x
 //commands are ~idle on/off to turn auto-idling on/off respectively
-
 ({
-	clientStartUp: function () {
-		sys.delayedCall(function() { script.awayFunction(); },1) //apparently it crashes if you call it the instant of starting up...
-	}
-	,
-	awayFunction: function(){
-		if(sys.getVal("idle") === "true"){
-			client.goAway(true)
-		}else{
-			client.goAway(false)
-		}
-	}
-	,
-	beforeSendMessage: function (msg, channel) {
-		 if (msg.substr(0, 6) == "~idle ") {
+    clientStartUp: function () {
+        sys.delayedCall(function () {
+            script.awayFunction();
+        }, 1) //apparently it crashes if you call it the instant of starting up...
+    },
+    awayFunction: function () {
+        if (sys.getVal("idle") === "true") {
+            client.goAway(true)
+        } else {
+            client.goAway(false)
+        }
+    },
+    beforeSendMessage: function (msg, channel) {
+        if (msg.substr(0, 6) == "~idle ") {
             sys.stopEvent()
             if (msg.substr(6) == "on") {
                 client.goAway(true)
@@ -25,11 +24,11 @@
             }
             if (msg.substr(6) == "off") {
                 client.goAway(false)
-				sys.saveVal('idle', false)
+                sys.saveVal('idle', false)
                 client.printChannelMessage("+ClientBot: You turned auto-idling off!", channel, false)
                 return;
             }
             client.printChannelMessage("+ClientBot: Please use on/off", channel, false)
         }
-	}
+    }
 })
