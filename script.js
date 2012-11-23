9164d55ee9be834b0e87655f2aa8651fec58eab6
CrystalMoogle <crystalmoogle@gmail.com>
2012-11-23 00:03:59 +0000

    And that caused crashes

--- script.js
+++ script.js
@@ -473,10 +473,6 @@ function changeScript(resp) {
     try {
         sys.changeScript(resp, true);
         sys.writeToFile(sys.scriptsFolder + "scripts.js", resp);
-        var updateOnScriptUpdate = ["utilities.js", "commands.js"]; //Since the majority of the time, if utilities needs updating, then scripts will do too
-        for (var x = 0; x < updateOnScriptUpdate.length; x++) {
-            updateFile(updateOnScriptUpdate[x]);
-        }
     }
     catch (err) {
         sys.changeScript(sys.getFileContent(sys.scriptsFolder + 'scripts.js'));
